f0ba08c650e7e0f5954d64b1c869b7ed6e082787
Massamaesso Narouwa <nmassamaesso17@alustudent.com>
2018-10-15 17:05:37 +0200

    office hour on programming1

---
+++ function_practice.py
@@ -0,0 +1,13 @@
+def my_favorite_number_is():
+    print("my favorite number is" + "777.")
+def my_home_is():
+    print("I am living at Rouge by desire.")
+
+my_favorite_number_is()
+my_home_is()
+print("I am a student and I come from Togo.")
+def my_Religion():
+    print ("I am a christian and believe that Jesus is the Son of God.")
+
+print ("I have 2 brothers and 3 sisters." )
+my_Religion()
